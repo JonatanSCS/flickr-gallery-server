7918ccb8c1f033baa346b63b5cb2e083e3356dea
jportella93 <jportella93@gmail.com>
2018-07-30 17:39:33 +0200

    feat: server running

---
+++ README.md
@@ -0,0 +1,25 @@
+# Get started
+**1.** Create a **.env** file in the root folder and and write your flickr API key following the **.env.example** file.
+
+
+
+**2.** Install dependencies:
+
+````
+yarn install
+````
+or
+````
+npm i
+````
+
+
+**3.** Start the server:
+
+````
+yarn run start
+````
+or
+````
+npm run start
+````
---
+++ controller.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const axios = require('axios');
+
+const apiKey = process.env.FLICKR_KEY
+
+module.exports.getPictures = async (ctx) => {
+  let pictureList = await axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=${apiKey}&per_page=10&format=json&nojsoncallback=1`)
+  pictureList = pictureList.data.photos.photo
+
+  const pictures = await Promise.all(pictureList.map(async p => {
+    let photo = await axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=${apiKey}&photo_id=${p.id}&format=json&nojsoncallback=1`)
+    return photo.data.sizes.size[1].source
+  }))
+  ctx.body = pictures
+}
